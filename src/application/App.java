9963950b74f2753536e9f41b82b67d85c829abab
Gilberto Junior <gagjunior@hotmail.com>
2022-10-07 00:39:21 -0300

    Inserindo dados

--- src/application/App.java
+++ src/application/App.java
@@ -1,36 +1,44 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class App {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-        
-        try{
+        PreparedStatement st = null;
+
+        try {
             conn = DB.getConnection();
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("select * from department");
+            st = conn.prepareStatement(
+                "INSERT INTO seller "
+                    +"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    +"VALUES "
+                    +"(?, ?, ?, ?, ?)");
 
-            System.out.println();
+            st.setString(1, "Gilberto Junior");
+            st.setString(2, "fulano@detal.com.br");
+            st.setDate(3, new java.sql.Date(sdf.parse("25/06/1987").getTime()));
+            st.setDouble(4, 3000.00);
+            st.setInt(5, 4);
 
-            while(rs.next()){
-                System.out.println(rs.getInt("Id") + ":" + rs.getString("Name"));
-            }
-        }
-        catch(SQLException e){
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! " + rowsAffected + " affected");
+
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally{
-            DB.closeResultSet(rs);
-            DB.closeStatment(stmt);
+        } catch (ParseException e){
+            e.printStackTrace();
+        } finally {
+            DB.closeStatment(st);
             DB.closeConnection();
         }
     }
